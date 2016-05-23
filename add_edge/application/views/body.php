d131df24abe1549113a38b2a35d35ab037a6df64
toxeek <avernopolis@gmail.com>
2016-05-23 13:01:50 +0100

    Update body.php

--- add_edge/application/views/body.php
+++ add_edge/application/views/body.php
@@ -3,13 +3,13 @@
 <head>
 <meta charset="UTF-8">
 <title>EDGE CONTROL PAGE</title>
-<link href="http://nagios01.priv.yospace.net/add_edge/css/jquery.filer.css" type="text/css" rel="stylesheet" />
-<link href="http://nagios01.priv.yospace.net/add_edge/css/themes/jquery.filer-dragdropbox-theme.css" type="text/css" rel="stylesheet" />
+<link href="http://example.com/add_edge/css/jquery.filer.css" type="text/css" rel="stylesheet" />
+<link href="http://example.com/add_edge/css/themes/jquery.filer-dragdropbox-theme.css" type="text/css" rel="stylesheet" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-<script src="http://nagios01.priv.yospace.net/add_edge/js/jquery.filer.min.js"></script>
+<script src="http://example.comt/add_edge/js/jquery.filer.min.js"></script>
 </head>
 <body>
 <nav id="myNavbar" class="navbar navbar-default navbar-inverse" role="navigation">
@@ -22,13 +22,13 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="http://nagios01.priv.yospace.net/add_edge/add/insert">Yospace</a>
+            <a class="navbar-brand" href="http://example.com/add_edge/add/insert">Yospace</a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <ul class="nav navbar-nav">
-                <li <?php if ($_pgheader == 'AddController') { echo 'class="active"'; } ?>><a href="http://nagios01.priv.yospace.net/add_edge/add/insert">add edges</a></li>
-                <li <?php if ($_pgheader == 'RemoveController') { echo 'class="active"'; } ?>><a href="http://nagios01.priv.yospace.net/add_edge/remove/delete">remove edges</a></li>
+                <li <?php if ($_pgheader == 'AddController') { echo 'class="active"'; } ?>><a href="http://example.com/add_edge/add/insert">add edges</a></li>
+                <li <?php if ($_pgheader == 'RemoveController') { echo 'class="active"'; } ?>><a href="http://example.com/add_edge/remove/delete">remove edges</a></li>
             </ul>
         </div>
     </div>
@@ -49,7 +49,7 @@
     </div>
     <div class="row">
         <div class="col-sm-4 col-md-4 col-lg-4">
-            <form action="<?php if ($_pgheader == 'AddController') { echo 'http://nagios01.priv.yospace.net/add_edge/add/uploader'; } elseif ($_pgheader == 'RemoveController') { echo 'http://nagios01.priv.yospace.net/add_edge/remove/uploader'; } else { echo 'noform'; } ?>" method="post" enctype="multipart/form-data">
+            <form action="<?php if ($_pgheader == 'AddController') { echo 'http://example.com/add_edge/add/uploader'; } elseif ($_pgheader == 'RemoveController') { echo 'http://example.com/add_edge/remove/uploader'; } else { echo 'noform'; } ?>" method="post" enctype="multipart/form-data">
                 <input type="file" name="files[]" id="filer_input" multiple="multiple">
                 <input type="submit" value="Submit">
             </form>
@@ -58,7 +58,7 @@
     <div class="row">
         <div class="col-sm-12">
             <footer>
-                <p>&copy; Copyright Yosapce Technologies</p>
+                <p>&copy; Copyright Example Technologies</p>
             </footer>
         </div>
     </div>
